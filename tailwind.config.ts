0c6c544a82f0edb0a4274071f56c3c4ee7205e4e
Farrel Fanny <farrelexamples@gmail.com>
2024-01-30 10:14:39 +0700

    add variables colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        DarkBlueDM: "hsl(209, 23%, 22%)",
+        VeryDarkBlueDM: "hsl(207, 26%, 17%)",
+        VeryDarkBlueLM: "hsl(200, 15%, 8%)",
+        DarkGrayLM: "hsl(0, 0%, 52%)",
+        VeryLightGrayLM: "hsl(0, 0%, 98%)",
+        White: "hsl(0, 0%, 100%)",
+      },
     },
   },
   plugins: [],
