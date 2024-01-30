75d2248766038988f92656a9b3ae466228a2ae1d
Farrel Fanny <farrelexamples@gmail.com>
2024-01-30 10:15:41 +0700

    make layout design for header bar

---
+++ src/app/ui/topbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaMoon } from "react-icons/fa";
+
+const Topbar = () => {
+  return (
+    <header className="flex flex-row items-center justify-between w-full py-8 px-4 bg-DarkBlueDM drop-shadow-lg">
+      <h1 className="text-base font-bold">Where in the world?</h1>
+      <div
+        id="DarkModeSwitch"
+        className="flex flex-row items-center justify-center gap-2"
+      >
+        <FaMoon size={14} />
+        <p className="text-xs font-semibold">Dark Mode</p>
+      </div>
+    </header>
+  );
+};
+
+export default Topbar;
