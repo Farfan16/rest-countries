25c002cb604c16f0f8c8b346750db5581b2b0ab5
Farrel Fanny <farrelexamples@gmail.com>
2024-02-03 21:22:54 +0700

    limit/slice how many country to show

--- src/app/ui/country.tsx
+++ src/app/ui/country.tsx
@@ -1,49 +1,60 @@
-import Image from "next/image";
-import React, { Suspense, useEffect, useState } from "react";
-import { fetchCountryData } from "../lib/data";
-import { CardSkeleton } from "./skeleton";
+"use client";
 
-const CountryWrapper = async () => {
+import Image from "next/image";
+import { fetchCountryData } from "../lib/data";
+
+const CountryWrapper = async ({
+  currentPage,
+  totalPages,
+}: {
+  currentPage: number;
+  totalPages: number;
+}) => {
   const getCountryData = await fetchCountryData();
   return (
     <div
       id="card-container"
-      className="flex flex-col items-center w-full gap-10 lg:flex-row lg:flex-wrap lg:justify-between"
+      className="flex flex-col items-center w-full gap-10 lg:flex-row lg:flex-wrap lg:items-stretch lg:justify-between"
     >
-      {getCountryData.map((dataObj: any) => (
-        <div
-          key={dataObj.index}
-          className="relative z-0 flex flex-col justify-center items-start w-4/5 bg-DarkBlueDM rounded-md drop-shadow-lg lg:w-1/5"
-        >
-          <div id="flag" className="flex items-stretch w-full h-full max-w-96">
-            <Image
-              src={dataObj.flags.svg}
-              width={500}
-              height={400}
-              alt={dataObj.flags.alt}
-              className="rounded-t-md h-full w-full object-cover aspect-[3/2]"
-            />
-          </div>
+      {getCountryData
+        .slice((currentPage - 1) * 8, currentPage * 8)
+        .map((dataObj: any) => (
           <div
-            id="desc"
-            className="flex flex-col items-start justify-center w-full p-4 gap-2"
+            key={dataObj.name.common}
+            className="relative z-0 flex flex-col justify-center items-start w-4/5 bg-DarkBlueDM rounded-md drop-shadow-lg lg:w-1/5"
           >
-            <h2 id="dataObj-name" className="mb-2 text-base font-bold">
-              {dataObj.name.common}
-            </h2>
-            <h3 id="population" className="font-medium text-sm">
-              Population:{" "}
-              <span className="font-light">{dataObj.population}</span>
-            </h3>
-            <h3 id="region" className="font-medium text-sm">
-              Region: <span className="font-light">{dataObj.region}</span>
-            </h3>
-            <h3 id="capital" className="font-medium text-sm">
-              Capital: <span className="font-light">{dataObj.capital}</span>
-            </h3>
+            <div
+              id="flag"
+              className="flex rounded-t-md items-stretch w-full h-full max-w-96"
+            >
+              <Image
+                src={dataObj.flags.svg}
+                width={500}
+                height={400}
+                alt={dataObj.flags.alt}
+                className="rounded-t-md object-cover aspect-[3/2]"
+              />
+            </div>
+            <div
+              id="desc"
+              className="flex flex-col items-start justify-center w-full p-4 gap-2"
+            >
+              <h2 id="dataObj-name" className="mb-2 text-base font-bold">
+                {dataObj.name.common}
+              </h2>
+              <h3 id="population" className="font-medium text-sm">
+                Population:{" "}
+                <span className="font-light">{dataObj.population}</span>
+              </h3>
+              <h3 id="region" className="font-medium text-sm">
+                Region: <span className="font-light">{dataObj.region}</span>
+              </h3>
+              <h3 id="capital" className="font-medium text-sm">
+                Capital: <span className="font-light">{dataObj.capital}</span>
+              </h3>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
