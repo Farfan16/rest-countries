7315caf5a391c7927158a29d132e8f0f2b80d4a8
Farrel Fanny <farrelexamples@gmail.com>
2024-02-03 21:31:55 +0700

    add pagination, suspense for country wrapper, and limit country shown for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Suspense } from "react";
 import CountryWrapper from "./ui/country";
 import Filter from "./ui/filter";
 import Search from "./ui/search";
 import { CardSkeleton } from "./ui/skeleton";
+import Pagination from "./ui/pagination";
+import { fetchDataPages } from "./lib/data";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { page?: string };
+}) {
+  const totalPages = await fetchDataPages();
+  const currentPage = Number(searchParams?.page) || 1;
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 px-4 pt-6 lg:px-16">
       <div className="flex flex-col w-full gap-10 lg:flex-row lg:justify-between lg:gap-0">
@@ -12,7 +21,13 @@ export default function Home() {
           <Filter />
         </div>
       </div>
-      <CountryWrapper />
+      <Suspense fallback={<CardSkeleton />}>
+        <CountryWrapper
+          currentPage={currentPage}
+          totalPages={Number(totalPages)}
+        />
+      </Suspense>
+      <Pagination totalPages={Number(totalPages)} />
     </main>
   );
 }
