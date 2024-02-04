d161538216b04700c5be187e7d2012f0a1497565
Farrel Fanny <farrelexamples@gmail.com>
2024-02-04 17:25:05 +0700

    add function to search country

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import React from "react";
 import { SlMagnifier } from "react-icons/sl";
+import { useDebouncedCallback } from "use-debounce";
 
-const Search = () => {
-  const handleSearch = (word: string) => {
-    console.log(word);
-  };
+const Search = ({ placeholder }: { placeholder: string }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((word: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (word) {
+      params.set("query", word);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  });
   return (
     <div className="flex flex-row justify-start items-center gap-6 w-full px-10 py-4 bg-DarkBlueDM rounded-md drop-shadow-md lg:w-1/2 lg:px-8">
       <SlMagnifier />
