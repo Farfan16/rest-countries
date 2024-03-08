25576bdfb837169591729a9986a4e58a53076146
Farrel Fanny <farrelexamples@gmail.com>
2024-03-08 20:25:09 +0700

    increase padding top

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -5,7 +5,7 @@ import Detail from "./ui/detail";
 const Page = ({ params }: { params: { id: string } }) => {
   const id = params.id;
   return (
-    <main className="px-5 py-8 lg:p-16 flex flex-col min-h-screen justify-start items-start text-VeryDarkBlueLM bg-VeryLightGrayLM dark:bg-VeryDarkBlueDM dark:text-White">
+    <main className="px-5 py-8 pt-28 lg:pt-24 lg:p-16 flex flex-col min-h-screen justify-start items-start text-VeryDarkBlueLM bg-VeryLightGrayLM dark:bg-VeryDarkBlueDM dark:text-White">
       <div className="flex h-fit w-full justify-start">
         <Link
           href="/"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({
   const totalPages = await fetchDataPages({ query, filter });
   const currentPage = Number(searchParams?.page) || 1;
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 px-4 py-6 lg:px-16 bg-VeryLightGrayLM dark:bg-VeryDarkBlueDM">
+    <main className="flex min-h-screen flex-col items-center gap-10 px-4 py-6 pt-28 lg:pt-24 lg:px-16 bg-VeryLightGrayLM dark:bg-VeryDarkBlueDM">
       <div className="flex flex-col w-full gap-10 lg:flex-row lg:justify-between lg:gap-0">
         <Search placeholder="Search for a country..." />
         <div className="relative z-10 flex justify-start w-full lg:justify-end">
