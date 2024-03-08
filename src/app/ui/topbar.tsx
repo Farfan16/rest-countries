708576f77f11d0c664318c89f8562ed2c581b366
Farrel Fanny <farrelexamples@gmail.com>
2024-03-08 20:25:24 +0700

    fixed header

--- src/app/ui/topbar.tsx
+++ src/app/ui/topbar.tsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from "./themeSwitcher";
 
 const Topbar = () => {
   return (
-    <header className="flex flex-row items-center justify-between w-full py-8 px-4 bg-white text-VeryDarkBlueLM dark:bg-DarkBlueDM dark:text-White drop-shadow-lg lg:px-16 lg:py-5">
+    <header className="flex flex-row items-center justify-between w-full fixed z-30 py-8 px-4 bg-white text-VeryDarkBlueLM dark:bg-DarkBlueDM dark:text-White drop-shadow-lg lg:px-16 lg:py-5">
       <h1 className="text-base font-bold">Where in the world?</h1>
       <ThemeSwitcher />
     </header>
